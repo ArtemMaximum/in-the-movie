8994f70fd557d39a527b30fefe9e87ab9bf88a9d
Artem <artem.habrahabrov@gmail.com>
2019-01-07 22:35:25 +0300

    redux reducers and another imports removed from project

--- src/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import movies from './modules/movies/reducer'
-import genres from './modules/genres/reducer'
-
-
-export default combineReducers({
-  movies,
-  genres,
-})
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,18 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import { Provider } from 'react-redux'
-import { RootTemplate, NotFoundPage } from './ui'
-import MoviesList from './modules/movies/containers/list'
-import MovieDetails from './modules/movies/containers/item'
-import TodoList from './modules/demo/TodoList'
-
-import configureStore from './configure-store'
-
-
-const store = configureStore()
+import { RootTemplate, NotFoundPage } from './ui';
+import MoviesList from './modules/movies/containers/list';
+import MovieDetails from './modules/movies/containers/item';
+import TodoList from './modules/demo/TodoList';
 
 export default () => (
   <RootTemplate sidebar="Sidebar">
-    <Provider store={store}>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route exact path="/movie/:movieId" component={MovieDetails} />
-        <Route exact path="/demo/mobx-todo" component={TodoList} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </Provider>
+    <Switch>
+      <Route exact path="/" component={MoviesList} />
+      <Route exact path="/movie/:movieId" component={MovieDetails} />
+      <Route exact path="/demo/mobx-todo" component={TodoList} />
+      <Route component={NotFoundPage} />
+    </Switch>
   </RootTemplate>
-)
+);
